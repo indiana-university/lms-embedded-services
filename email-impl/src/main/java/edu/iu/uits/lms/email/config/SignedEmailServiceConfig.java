c72a0092e465da2c257c26e99f8a21f0e6d37889
chmaurer <chmaurer@iu.edu>
2020-08-25 08:55:52 -0400

    LMSA-6905 Make properties files for the specific service areas (canvas, redis, database, etc)

--- email-impl/src/main/java/edu/iu/uits/lms/email/config/SignedEmailServiceConfig.java
+++ email-impl/src/main/java/edu/iu/uits/lms/email/config/SignedEmailServiceConfig.java
@@ -4,15 +4,9 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @ConfigurationProperties(prefix = "sis.rest")
-@PropertySource(value = {"classpath:env.properties",
-      "classpath:default.properties",
-      "${app.fullFilePath}/lms.properties",
-      "${app.fullFilePath}/protected.properties",
-      "${app.fullFilePath}/security.properties"}, ignoreResourceNotFound = true)
 @Getter
 @Setter
 public class SignedEmailServiceConfig {

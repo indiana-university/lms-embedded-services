25bbf53daeb362accb017bfe1b3bd6a5980a0183
Damian Sobieralski <dsobiera@iu.edu>
2023-05-02 14:13:02 -0400

    LMSA-7726 LMSA-7727 LMSA-7728 LMSA-7729 - removed undeed import

--- lti-framework/src/main/java/edu/iu/uits/lms/lti/config/LtiRestConfiguration.java
+++ lti-framework/src/main/java/edu/iu/uits/lms/lti/config/LtiRestConfiguration.java
@@ -39,7 +39,6 @@ import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.annotation.Order;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;

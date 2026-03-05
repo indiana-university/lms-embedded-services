2cf4e61dec78b20d2ee929f31f0c4e853fe67658
Wagner <wagnermr@iu.edu>
2026-03-05 08:25:01 -0800

    LMSA-9904 upgrade test

--- iu-custom-services/src/test/java/edu/iu/uits/lms/iuonly/ToolPermissionServiceTest.java
+++ iu-custom-services/src/test/java/edu/iu/uits/lms/iuonly/ToolPermissionServiceTest.java
@@ -37,29 +37,30 @@ import edu.iu.uits.lms.iuonly.model.tps.*;
 import edu.iu.uits.lms.iuonly.repository.*;
 import edu.iu.uits.lms.iuonly.services.ToolPermissionService;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
-@ExtendWith(MockitoExtension.class)
+@SpringBootTest(classes = ToolPermissionService.class)
 public class ToolPermissionServiceTest {
 
-    @Mock
+    @MockitoBean
+    private AuthToolRepository authToolRepository;
+    @MockitoBean
     private AuthPermissionRepository authPermissionRepository;
-    @Mock
+    @MockitoBean
     private AuthUserPermissionRepository authUserPermissionRepository;
-    @Mock
+    @MockitoBean
     private AuthUserRepository authUserRepository;
-    @Mock
+    @MockitoBean
     private AuthUserPermissionPropertyRepository authUserPermissionPropertyRepository;
 
-    @InjectMocks
+    @Autowired
     private ToolPermissionService toolPermissionService;
 
     @Test

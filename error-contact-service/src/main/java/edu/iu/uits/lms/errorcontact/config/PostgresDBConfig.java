6519a2e406e03ad74f7e753cdaf23018aed87be0
chmaurer <chmaurer@iu.edu>
2026-03-16 09:41:55 -0400

    LMSA-10071 - Name the configuration to avoid conflicts

--- error-contact-service/src/main/java/edu/iu/uits/lms/errorcontact/config/PostgresDBConfig.java
+++ error-contact-service/src/main/java/edu/iu/uits/lms/errorcontact/config/PostgresDBConfig.java
@@ -48,7 +48,7 @@ import javax.sql.DataSource;
 import java.util.HashMap;
 import java.util.Map;
 
-@Configuration
+@Configuration("ErrorContactPostgresDBConfig")
 @EnableJpaRepositories(
         entityManagerFactoryRef = "errorContactPostgresdbEntityMgrFactory",
         transactionManagerRef = "errorContactPostgresdbTransactionMgr",
